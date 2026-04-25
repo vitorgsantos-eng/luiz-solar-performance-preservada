a9008593b71d447ae67c4a75751b6cab75384717
vitorgsantos-eng <vitorsgoncalves89@gmail.com>
2026-04-25 16:12:41 -0300

    Add Instagram URL constant

--- src/components/luiz/constants.ts
+++ src/components/luiz/constants.ts
@@ -4,3 +4,4 @@ export const WHATSAPP_MESSAGE = "Olá, gostaria de agendar um diagnóstico solar
 export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(WHATSAPP_MESSAGE)}`;
 export const EMAIL = "contato@luizsolar.com.br";
 export const INSTAGRAM = "@luizsolar";
+export const INSTAGRAM_URL = "https://instagram.com/luizsolar";
