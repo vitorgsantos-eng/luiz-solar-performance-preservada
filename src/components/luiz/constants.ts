e6d8754c2dfaddbc9554c13fee2c20a8faedf359
vitorgsantos-eng <vitorsgoncalves89@gmail.com>
2026-04-26 00:18:09 +0000

    Corrige Instagram da Luiz Solar

--- src/components/luiz/constants.ts
+++ src/components/luiz/constants.ts
@@ -3,5 +3,5 @@ export const WHATSAPP_DISPLAY = "(71) 98741-1172";
 export const WHATSAPP_MESSAGE = "Olá, gostaria de agendar um diagnóstico solar";
 export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(WHATSAPP_MESSAGE)}`;
 export const EMAIL = "contato@luizsolar.com.br";
-export const INSTAGRAM = "@luizsolar";
-export const INSTAGRAM_URL = "https://instagram.com/luizsolar";
+export const INSTAGRAM = "@luiz.solar";
+export const INSTAGRAM_URL = "https://www.instagram.com/luiz.solar/";
