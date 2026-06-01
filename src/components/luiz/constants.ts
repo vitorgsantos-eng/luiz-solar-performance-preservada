f34d9f17ae24fab80c8ced9d42ee7c0affd245e9
Vitor Gonçalves dos Santos <vitorsgoncalves89@gmail.com>
2026-06-01 09:55:19 -0300

    Atualiza telefone do WhatsApp da Luiz Solar

--- src/components/luiz/constants.ts
+++ src/components/luiz/constants.ts
@@ -1,5 +1,5 @@
-export const WHATSAPP_NUMBER = "5571987411172";
-export const WHATSAPP_DISPLAY = "(71) 98741-1172";
+export const WHATSAPP_NUMBER = "5571981655959";
+export const WHATSAPP_DISPLAY = "(71) 98165-5959";
 export const WHATSAPP_MESSAGE = "Olá, gostaria de agendar um diagnóstico solar";
 export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(WHATSAPP_MESSAGE)}`;
 export const EMAIL = "contato@luizsolar.com.br";
